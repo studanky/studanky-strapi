126b7a8f403b3d26093386ab019bf62ccb1e4250
Jakub Smolik <smolikja@protonmail.com>
2026-05-31 12:55:38 +0200

    feat: flow range component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,43 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ConfigFlowRange extends Struct.ComponentSchema {
+  collectionName: 'components_config_flow_ranges';
+  info: {
+    displayName: 'flow-range';
+  };
+  attributes: {
+    max_lps: Schema.Attribute.Decimal &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          min: 0;
+        },
+        number
+      >;
+    min_lps: Schema.Attribute.Decimal &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          min: 0;
+        },
+        number
+      >;
+    scale: Schema.Attribute.Integer &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          max: 5;
+          min: 1;
+        },
+        number
+      >;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'config.flow-range': ConfigFlowRange;
+    }
+  }
+}
